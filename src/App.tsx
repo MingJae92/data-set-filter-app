9ac1f186bcbb14048a15051eaf07ebe190564db9
Ming Chi <wong_mingchi@yahoo.co.uk>
2025-03-12 23:41:35 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Datadisplay from "./component/dataDisplay/Datadisplay";
+
+function App() {
+
+
+  return (
+    <>
+      <div><Datadisplay/></div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/dataDisplay/Datadisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Datadisplay() {
+  return (
+    <div>Datadisplay</div>
+  )
+}
+
+export default Datadisplay
