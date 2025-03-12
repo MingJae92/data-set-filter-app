18562caa4386a52ee9331add966e6bb62d133270
Ming Chi <wong_mingchi@yahoo.co.uk>
2025-03-12 23:52:58 +0000

    Data rendered

--- src/component/dataDisplay/Datadisplay.tsx
+++ src/component/dataDisplay/Datadisplay.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import { Product } from "../../types/dataDisplay.types"
+import { useEffect, useState, useCallback } from "react"
+import axios from "axios"
 
 function Datadisplay() {
+    const [products, setProducts]=useState<Product[]>([])
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<boolean>(false)
+
+    const dataFetch = useCallback(async () => {
+        try {
+          setLoading(true);
+          const ecommerceData = await axios.get<Product[]>(
+            "https://fakestoreapi.com/products/"
+          );
+          setProducts(ecommerceData.data);
+          setLoading(false);
+          console.log("Data refreshed:", ecommerceData.data);
+        } catch (error) {
+          setError(true);
+          console.log(error);
+        }
+      }, []);
+
+    useEffect(()=>{
+        dataFetch()
+    },[])
+
+    if(error){
+        return <div>Error loading...</div>
+    }
+
+    if(loading){
+        return <div>Loading...</div>
+    }
+
   return (
-    <div>Datadisplay</div>
+    <div>Datadisplay
+        {products.map((item) => (
+        <div key={item.id}>
+          <img src={item.image} alt={item.title} width="100" />
+          <h3>{item.title}</h3>
+          <p>${item.price}</p>
+          <p>{item.category}</p>
+          <p>
+            ⭐ {item.rating.rate} ({item.rating.count} reviews)
+          </p>
+        </div>
+      ))}
+    </div>
   )
 }
 
